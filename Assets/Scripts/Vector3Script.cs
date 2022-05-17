ce3d1dc91e894894ea9ce1bebb744a3f0530ef68
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-05-17 10:45:14 -0300

    finished exercise vector3

---
+++ Assets/Scripts/Vector3Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+    public Vector3 miVector;
+    public GameObject myobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myobject.transform.position = miVector;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
